33354d7c1cc1e7a00d1d7640f0341975cdb9b469
Alex808r <fromxlam@gmail.com>
2021-04-21 13:41:40 +0300

    add validates/ add errors message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
 
   #метод для создания нового поста posts/new
   def new
+    @post = Post.new
   end
 
   def show
@@ -15,9 +16,13 @@ class PostsController < ApplicationController
   def create
     #render plain: params[:post].inspect для проверки вывода информации из формы [:post] - назвние формы
     @post = Post.new(post_params)
-    @post.save
 
-    redirect_to @post # вызовет метод show и переведет пользователя на новую страницу
+    if(@post.save)
+      redirect_to @post # вызовет метод show и переведет пользователя на новую страницу
+    else
+      render 'new' # перезагрузка страницы если не прошла валидация
+    end
+
   end
 
   private
